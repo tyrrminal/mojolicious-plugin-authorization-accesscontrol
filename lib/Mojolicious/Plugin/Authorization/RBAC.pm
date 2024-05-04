d027b122c74e2c0190aaf198957bfeb1e3173a80
tyrrminal <mark@tyrrminal.dev>
2024-05-04 10:35:01 -0400

    initial commit

---
+++ lib/Mojolicious/Plugin/Authorization/RBAC.pm
@@ -0,0 +1,9 @@
+package Mojolicious::Plugin::Authorization::RBAC;
+use v5.26;
+use feature qw(isa);
+
+#ABSTRACT: provides Role-Based Access Control for Mojolicious applications
+
+use Mojo::Base 'Mojolicious::Plugin';
+
+1;
---
+++ lib/Mojolicious/Plugin/Authorization/RBAC/Privilege.pm
@@ -0,0 +1,9 @@
+package Mojolicious::Plugin::Authorization::RBAC::Privilege;
+use v5.26;
+
+# ABSTRACT: Defines an RBAC Privilege
+
+use Object::Pad;
+
+class Mojolicious::Plugin::Authorization::RBAC::Privilege :strict(params) {
+}

6229f8d312daed2b3cb653ec256579ac04649db5
tyrrminal <mark@tyrrminal.dev>
2024-05-13 16:58:23 -0400

    update documentation

--- lib/Mojolicious/Plugin/Authorization/RBAC.pm
+++ lib/Mojolicious/Plugin/Authorization/RBAC.pm
@@ -51,12 +51,12 @@ required:
     ]);
     any_role(
       priv(Book => 'add'),
-      priv(Book => 'read',   [qw(owned)]),
-      priv(Book => 'read',   [qw(public)]),
-      priv(Book => 'search', [qw(owned)]),
-      priv(Book => 'search', [qw(public)]),
-      priv(Book => 'edit',   [qw(owned)]),
-      priv(Book => 'remove,  [qw(owned)]),
+      priv(Book => 'read',   {owned  => true}),
+      priv(Book => 'read',   {public => true}),
+      priv(Book => 'search', {owned  => true}),
+      priv(Book => 'search', {public => true}),
+      priv(Book => 'edit',   {owned  => true}),
+      priv(Book => 'remove,  {owned  => true}),
     );
   }
 
@@ -67,11 +67,12 @@ required:
     ...
   }
   
-In this example, we've aded some attributes to our rules. Attributes are simple
-strings which express additional qualifications of a rule. All attributes of a 
-rule must be met for the rule to pass, but multiple rules can be created with
-different attributes -- if any rule passes, the privilege is granted. Therefore,
-in this example, the edit operation will fail, except in any of the following cases:
+In this example, we've aded some restrictions to our rules. Restrictions are 
+key/value pairs which impose additional qualifications on a privilege. All 
+restrictions of a privilege must be met for the it to pass, but multiple rules 
+can be created with different restrictions -- if any passes, the privilege is 
+considered granted. Therefore, in this example, the edit operation will fail, 
+except in any of the following cases:
 
 =over
 
@@ -84,12 +85,12 @@ in this example, the edit operation will fail, except in any of the following ca
 =back
 
 This works well enough, but can be a bit verbose, especially if you need to pass
-multiple permissons checks
+multiple permissons checks.
 
   BEGIN {
     any_role(
-      priv(Book => 'edit', [qw(owner)]),
-      priv(Book => 'edit', [qw(public)]),
+      priv(Book => 'edit', {owner => true}),
+      priv(Book => 'edit', {public => true}),
     );
     attr_cb(Book => sub($c, $ctx) {
       +{
@@ -106,8 +107,9 @@ multiple permissons checks
   }
 
 By registering an attribute callback with L</attr_cb> and establishing an authz
-L<context|/authz.context-($value)> prior to checking privileges, the entity's attributes 
-will be calculated for you.
+L<context|/authz.context-($value)> prior to checking privileges, the entity's 
+attributes will be dynamically determined just prior to evalauating the 
+privileges.
 
 Finally, the L<yield|/authz.yield-($cb,-@args)> method can be used to cleanly
 isolate the code that obtains instances of protected data from the code that
@@ -119,8 +121,8 @@ information.
 
   BEGIN {
     any_role(
-      priv(Book => 'edit', [qw(owner)]),
-      priv(Book => 'edit', [qw(public)]),
+      priv(Book => 'edit', {owner => true}),
+      priv(Book => 'edit', {public => true}),
     );
     attr_cb(Book => sub($c, $ctx) {
       +{
@@ -171,28 +173,28 @@ of each request, ensuring that they are always up-to-date.
   $app->hook(before_dispatch => sub($c) {
     foreach my $g (db->model('group')->all) {
       $c->authz->role($g->name => [
-        map { priv(Book => $_->permission, [
-          sprintf('book_id[%d]', $_->book_id)
-        ]) } $g->book_permissions->all
+        map { priv(Book => $_->permission, {
+          book_id => $_->book_id
+        }) } $g->book_permissions->all
       ]);
     }
   });
 
   BEGIN {
     any_role(
-      priv(Book => 'edit', [qw(owner)]),
-      priv(Book => 'edit', [qw(public)]),
+      priv(Book => 'edit', {owner => true}),
+      priv(Book => 'edit', {public => true}),
     );
-    # match the attributes created in the dynamic rules with per-Book attributes
+    # match the restrictions in the dynamic rules with Book-specific attributes
     attr_cb(Book => sub($c, $ctx) {
       +{
-        sprintf('book_id[%d]', $ctx->id) => 1,
+        book_id => $ctx->id,
       }
     })
   }
 
 Now, when the privileges are checked, the user's group must match, and the 
-C<book_id[n]> attribute must also match, allowing for fine-grained privilege
+C<book_id> attribute must also match, allowing for fine-grained privilege
 application.
 
 =head1 DESCRIPTION
@@ -224,9 +226,10 @@ simplest is the L<authz.permitted|/authz.permitted-($resource,-$action-[,-$attrs
 helper, which can be optionally provided an C<attributes> array or combined with 
 a data context via the L<authz.context|/authz.context-($value)> helper and the 
 corresponding L</attr_cb> callback. Or, you can use 
-L<authz.yield|/authz.yield-($cb,-@args)> to invoke the exception-based workflow 
-and ensure that privileged data objects are only made available to application 
-logic once privilege checks are passed.
+L<authz.yield|/authz.yield-($cb,-@args)> to invoke the 
+L<Mojolicious::Plugin::Authorization::RBAC::YieldResult> workflow and ensure 
+that privileged data objects are only made available to application logic once 
+privilege checks are passed.
 
 =cut
 
@@ -391,7 +394,8 @@ sub register($self, $app, $args) {
 
 =head2 authz.priv ($resource, $action [, $attrs])
 
-Identical to L</priv>
+Functionally identical to L</priv>. Provided so that the import can be skipped
+in contexts where only dynamic privileges are being created.
 
 =cut
 
@@ -491,32 +495,17 @@ Logs a "granted" or "denied" message to the selected C<Mojo::Log> instance
     }
   );
 
-=head2 authz.yield ($cb, @args)
+=head2 authz.yield ($get_value_cb, $resource, $action, $attrs)
 
-Produce a guarded data value, if permitted. The first argument callback should
-perform the minimum necessary to obtain the data value, and return it. This
-value is then automatically set as the request's authorization context, and then
-the privilege rules are evaluated. If the callback returns C<undef>, an 
-C<Authorization::RBAC::NullYield> exception is thrown.
+Yield a protected data value, if permitted. The first argument callback should
+perform the minimum necessary to obtain the data value and immediately return it.
+This value is then passed through C<attr_cb> and the dynamic attributes are merged
+with the static C<$attrs> Hash passed in. Then 
+L<authz.permitted|/authz.permitted-($resource,-$action-[,-$attrs])> is called
+to check the resource/action/attrs against all registered privileges.
 
-C<@args> must be in one of two possible formats. For a single privilege check,
-C<($resource, $action [, $attrs])>, just like the arguments to 
-L<authz.permitted|/authz.permitted-($resource,-$action-[,-$attrs])>
-
-However, in some cases, multiple privileges should be checked before the data
-should be yielded. For example, a photo gallery app might check both the 
-C<media.view> and C<media.view.full_size> privileges before rendering a 
-full-size image. In these cases, an array of 
-L<Privileges|Mojolicious::Plugin::Authorization::RBAC::Privilege> (as from 
-L</priv>) may be passed instead -- however, please note that only the
-L<resource|Mojolicious::Plugin::Authorization::RBAC::Privilege/resource> and
-L<action|Mojolicious::Plugin::Authorization::RBAC::Privilege/action> properties
-are respected on these objects.
-
-An authorization log message is emitted for each privilege checked.
-
-If all privilege checks pass, the data is returned. If any check fails, an
-C<Authorization::RBAC::Failure> exception is thrown.
+Returns a L<Mojolicious::Plugin::Authorization::RBAC::YieldResult>, upon which
+callbacks may be registered to handle the result of the authorization check.
 
 =cut
   

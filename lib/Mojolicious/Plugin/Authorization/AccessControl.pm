f89cbdb695c89feddb5038b0f124f8a520ec02fd
tyrrminal <mark@tyrrminal.dev>
2024-05-16 17:19:28 -0400

    rework for simplicity and effectiveness

--- lib/Mojolicious/Plugin/Authorization/AccessControl.pm
+++ lib/Mojolicious/Plugin/Authorization/AccessControl.pm
@@ -4,8 +4,9 @@ use warnings;
 
 use Mojo::Base 'Mojolicious::Plugin';
 
-use Authorization::AccessControl qw(ac);
+use Authorization::AccessControl qw(acl);
 use Readonly;
+use Syntax::Keyword::Try;
 
 use experimental qw(signatures);
 
@@ -15,7 +16,9 @@ sub register($self, $app, $args) {
   my $prefix = $args->{prefix} // $DEFAULT_PREFIX;
   my $stash_ac = "_$prefix.request.accesscontrol";
 
-  my $get_roles = $args->{get_roles} // sub($c) { $c->current_user_roles };
+  my $get_roles = sub($c) { $c->current_user_roles };
+  $get_roles = $args->{get_roles}//sub($c){[]} if(exists($args->{get_roles}));
+  die("get_roles must be a CODEREF/anonymous subroutine") if(defined($get_roles) && ref($get_roles) ne 'CODE');
   
   my $log_f = sub($m) { $app->log->info($m) };
   if(exists($args->{log})) {
@@ -26,56 +29,57 @@ sub register($self, $app, $args) {
     } else { $log_f = sub {} }
   }
 
+  acl->hook(on_permit => sub ($ctx) { $log_f->("[Authorization::AccessControl] Granted: $ctx") });
+  acl->hook(on_deny   => sub ($ctx) { $log_f->("[Authorization::AccessControl] Denied: $ctx") });
+
   my $get_ac = sub($c) {
-    my $ac = ac;
+    my $ac = acl;
     if($c->tx->connection) {
-      $c->stash($stash_ac => ac->clone) unless(defined($self->stash($stash_ac)));
-      $ac = $self->stash($stash_ac);
+      $c->stash($stash_ac => $ac->clone) unless(defined($c->stash($stash_ac)));
+      $ac = $c->stash($stash_ac);
     }
     return $ac;
-  }
+  };
+
+  $app->helper("$prefix.acl" => sub ($c) {
+    return $get_ac->($c);
+  });
 
   $app->helper("$prefix.role" => sub ($c, @params) {
     return $get_ac->($c)->role(@params);
   });
 
-  $app->helper("$prefix.grant" => sub ($c, @params) {
-    return $get_ac->($c)->grant(@params);
+  my @get_attrs;
+  $app->helper("$prefix.dynamic_attrs" => sub ($c, @params) {
+    my $get_attrs = { handler => pop(@params) };
+    $get_attrs->{resource} = shift(@params) if(@params);
+    $get_attrs->{action}   = shift(@params) if(@params);
+    push(@get_attrs, $get_attrs);
   });
 
-  my $get_dyn_attrs = sub ($c, $resource, $action) {
-    my @prefixes = ($DEFAULT_PREFIX);
-    unshift(@prefixes, $prefix) if($prefix ne $DEFAULT_PREFIX);
-
-    my $n = 'extract_attrs';
-    foreach ([$resource, $action], [$resource], []) {
-      my $cm = join('_', ($n, map { s/[^a-zA-Z0-9_]/_/gr } $_->@*));
-      if($c->can($cm)) {
-        try { return sub($ctx) { $c->$cm($ctx) } } catch($e) { $c->log->warn($e); return {} }
-      }
-      foreach my $p (@prefixes) {
-        my $ri = join('_', $_->@*);
-        my $hm = join('.', ($p, $n, ($ri ? ($ri) : ()) ));
-        if(my $h = $c->app->renderer->get_helper($hm)) {
-          try { return sub($ctx) { $h->($c, $ctx) } } catch($e) { $c->log->warn($e); return {} }
-        }
-      }
-    }
-    return undef;
+  my $get_get_attrs = sub ($resource, $action) {
+    my @c = @get_attrs;
+    @c = grep { !defined($_->{resource}) || $_->{resource} eq $resource } @c if(defined($resource));
+    @c = grep { !defined($_->{action}) || $_->{action} eq $action } @c if(defined($action));
+    @c = sort { defined($b->{resource}) + defined($b->{action}) - (defined($a->{resource}) + defined($a->{action})) } @c;
+    return ($c[0]//{})->{handler};
   };
 
-  $app->helper("$prefix.predicate" => sub ($c, $resource = undef, $action = undef) {
-    my $roles = $c->tx->connection ? $get_roles->($c) : [];
+  $app->helper("$prefix.request" => sub ($c, $resource = undef, $action = undef) {
+    my $roles = [];
+    try { $roles = $get_roles->($c) } catch($e) {}
     
-    my $pred = $get_ac->($c)->roles($roles->@*);
-    $pred = $pred->perform($action) if(defined($action));
-    $pred = $pred->on_resource($resource) if(defined($resource));
-    my $f = $get_dyn_attr_f->($c, $resource, $action);
-    $pred = $pred->with_get_attrs($f) if($f);
+    my $req = $get_ac->($c)->request->with_roles($roles->@*);
+    $req = $req->with_action($action) if(defined($action));
+    $req = $req->with_resource($resource) if(defined($resource));
+    if(my $f = $get_get_attrs->($resource, $action)) {
+      $req = $req->with_get_attrs(sub($ctx){ $f->($c, $ctx) });
+    }
 
-    return $pred;
-  })
+    return $req;
+  });
 }
+
 
 =head1 AUTHOR
 
